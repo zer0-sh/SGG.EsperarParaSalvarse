4c99441e025b1271ae6aad6c044361975346939b
zer0-sh <steven.jm357@gmail.com>
2022-12-16 17:36:39 -0500

    scriptsdatosbd

---
+++ BD/ips.sql
@@ -0,0 +1,8 @@
+insert into ips values (2133432, 'Vieja IPS', 'Alta');
+insert into ips values (1230118, 'Servisalud IPS', 'Media');
+insert into ips values (3247923, 'Clinica Valle del Socorro', 'Alta');
+insert into ips values (1274314, 'Hospital Universitario', 'Media');
+insert into ips values (0000001, 'Hospital municipal', 'Baja');
+insert into ips values (0000324, 'SISBEN', 'Baja');
+insert into ips values (02349239, 'Nueva IPS', 'Alta');
+insert into ips values (23434432, 'Nuevo Nacer IPS', 'Baja')
---
+++ BD/servicios.sql
@@ -0,0 +1,15 @@
+insert into servicios values(0001, 'Interrupcion Voluntaria de embarazo', 2133432);
+insert into servicios values(0002, 'Cirugia plastica', 2133432);
+insert into servicios values(0003, 'Otorrinolaringología', 23434432);
+insert into servicios values(0004, 'Ginecología', 1230118);
+insert into servicios values(0005, 'Odontología', 1230118);
+insert into servicios values(0006, 'Oncología', 1230118);
+insert into servicios values(0007, 'Atención Básica', 3247923);
+insert into servicios values(0008, 'Peditría', 02349239);
+insert into servicios values(0009, 'Vacunación', 0000324);
+insert into servicios values(0010, 'Psicología', 0000324);
+insert into servicios values(0011, 'Tratamientod de consumo de SPA', 02349239);
+insert into servicios values(0012, 'Nutrición', 0000001);
+insert into servicios values(0013, 'Laboratorio', 0000001);
+insert into servicios values(0014, 'Hospitalización', 1274314);
+insert into servicios values(0015, 'Optometría', 1274314)
